e43910e6c3378ac77b2b4fb47aaef1a54b41c805
linggeli7 <linggeli7@gmail.com>
2014-05-21 22:36:12 -0700

    Update views.py

--- tutorme/views.py
+++ tutorme/views.py
@@ -2,8 +2,11 @@
 This is the view file: index is called whenever the url is hit.
 '''
 
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import auth
+from django.core.context_processors import csrf
 import datetime
 from models import Tutor
 
@@ -16,54 +19,48 @@ from settings import MEDIA_ROOT
 def home(request):
   return render(request, 'home.html')
 
-def language_partners(request):
-  tutors = Tutor.objects.all()
-  return render(request, 'language_partners.html', {'tutors':tutors})
+""" Authentication Package
+"""
 
-def valid_signup(request):
-  if request.POST.get('email') and request.POST.get('password') and request.POST.get('password_repeat') and request.POST.get('name') and request.POST.get('college'):
-    if request.POST.get('password') == request.POST.get('password_repeat'):
-      if Tutor.objects.filter(email=request.POST['email']).count() == 0:
-        return True
-  return False
-
-def valid_login(request):
-  if request.POST.get('email') and request.POST.get('password'):
-    if Tutor.objects.filter(email=request.POST['email'], password=request.POST['password']).count() == 1:
-      return True
-    if Tutor.objects.filter(email=request.POST['email'], password=request.POST['password']).count() > 1:
-      assert False
-  return False
+def dashboard(request):
+  """ Only logged in users can access
+      Major functions to be added on this page 
+  """
+  if not request.user.is_authenticated():
+        return HttpResponseRedirect('/login')
+  return render(request, 'dashboard.html')
 
 def signup(request):
-  if request.method == 'GET':
-    error_message = request.GET.get('error_message', '')
-    return render(request, 'signup.html', {'error_message': error_message})
-  else:
-    if valid_signup(request):
-      suffix = request.FILES.get('image').name.split('.')[-1] # get the image suffix
-      name = request.POST.get('email') + '.' + suffix
-      file_name = MEDIA_ROOT + '/images/' + name
-      image_url = '/media/images/' + name
-      with open(file_name, 'w') as destination:
-        for chunk in request.FILES.get('image').chunks():
-          destination.write(chunk)
-      Tutor.objects.create(
-                           image_url=image_url,
-                           email=request.POST.get('email'),
-                           password=request.POST.get('password'),
-                           name=request.POST.get('name'),
-                           college=request.POST.get('college'))
-      return redirect('/language_partners')
-    else:
-      return redirect('/signup?error_message=Invalid credentials.')
+  """ Needs to extend the user model and creation form
+  """
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('/dashboard')
+
+  args = {}
+  args.update(csrf(request))
+
+  args['form'] = UserCreationForm()
+  return render_to_response('signup.html',args)
 
 def login(request):
-  if request.method == 'GET':
-    error_message = request.GET.get('error_message', '')
-    return render(request, 'login.html', {'error_message': error_message})
+  c = {}
+  c.update(csrf(request))
+  return render_to_response('login.html', c)
+  
+def auth_view(request):
+  username = request.POST.get('username', '')
+  password = request.POST.get('password', '')
+  user = auth.authenticate(username=username, password=password)
+
+  if user is not None:
+    auth.login(request, user)
+    return HttpResponseRedirect('/dashboard')
   else:
-    if valid_login(request):
-      return redirect('/language_partners')
-    else:
-      return redirect('/login?error_message=Invalid login.')
+    return HttpResponseRedirect('/login')
+
+def logout(request):
+  auth.logout(request)
+  return HttpResponseRedirect('/')
