3a1ccd0a4ec331ea47a46f86d8baafd9106fa1e8
linggeli7 <linggeli7@gmail.com>
2014-05-25 19:13:55 -0700

    Update urls.py

--- tutorme/urls.py
+++ tutorme/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-import settings_dev
+import settings
 
 urlpatterns = patterns('',
     url(r'^$', 'tutorme.views.home'),
@@ -15,9 +15,10 @@ urlpatterns = patterns('',
     url(r'^auth/$', 'tutorme.views.auth_view'),
     url(r'^dashboard/$', 'tutorme.views.dashboard'),
     url(r'^logout/$', 'tutorme.views.logout'),
+    url(r'^profile/$', 'tutorme.views.profile'),
+    url(r'^search/$', 'tutorme.views.search'),
 
-    (r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings_dev.MEDIA_ROOT }),
-
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT }),
 
     url(r'^admin/', include(admin.site.urls)),
 )
